3059a321ff6bd6960709502adb556d046e0428df
ToltalTouch <micaeldoamaral@gmail.com>
2025-08-21 10:32:20 -0300

    funcao de controle por linha e tratamento de erro

--- xlxs_json.py
+++ xlxs_json.py
@@ -3,33 +3,49 @@ import logging
 import glob
 import os
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 pasta = os.path.dirname(os.path.abspath(__file__))
 arquivos = glob.glob(os.path.join(pasta, "*.xlsx"))
 
-for arquivo in arquivos:
-    logging.info(f"Lendo arquivo: {arquivo}")
+def processar_linha(row):
+    for col in row.index:
+        valor =row[col]
+        if pd.notnull(valor):
+            if pd.notnull(valor):
+                if pd.api.types.is_datetime64_any_dtype(df[col]):
+                    row[col] = valor.strftime("%d/%m/%Y")
+                elif pd.api.types.is_numeric_dtype(df[col]):
+                    row[col] = str(int(valor)) if float(valor).is_integer() else str(valor)
+    return row
 
-    df = pd.read_excel(arquivo, sheet_name=0)
-    
-    for col in df.select_dtypes(include=["datetime", "datetimetz"]).columns:
-        df[col] = df[col].dt.strftime("%d/%m/%Y")
+def main():
+    for arquivo in arquivos:
+        logging.info(f"Lendo arquivo: {arquivo}")
 
-    for col in df.select_dtypes(include=["number"]).columns:
-        df[col] = df[col].apply(
-            lambda x: str(int(x)) if pd.notnull(x) and float(x).is_integer() else str(x)
-        )
-
-    json_data = df.to_json(orient="records",
-                        force_ascii=False,
-                        indent=4
-                        )
-
-    json_data = json_data.replace("\\/", "/")
-
-    nome_json = os.path.splitext(os.path.basename(arquivo))[0] + ".json"
-    caminho_json = os.path.join(pasta, nome_json)
-
-    with open(caminho_json, "w", encoding="utf-8") as file:
-        file.write(json_data)
+        df = pd.read_excel(arquivo, sheet_name=0)
+        df = df.apply(processar_linha, axis=1)
         
-    print("JSON gerado com sucesso")
+        for i in range(len(df)):
+            try:
+                logging.info(f"Linha {i} concluida")
+            except Exception as e:
+                logging.error(f"Erro ao processar linha {i}: {e}")
+
+        json_data = df.to_json(orient="records",
+                            force_ascii=False,
+                            indent=4
+                            )
+
+        json_data = json_data.replace("\\/", "/")
+
+        nome_json = os.path.splitext(os.path.basename(arquivo))[0] + ".json"
+        caminho_json = os.path.join(pasta, nome_json)
+
+        with open(caminho_json, "w", encoding="utf-8") as file:
+            file.write(json_data)
+            
+        print("JSON gerado com sucesso")
+        
+if __name__ == "__main__":
+    main()
