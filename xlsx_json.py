daa953708018826f9d8e25ae600dc1aa570e350e
ToltalTouch <micaeldoamaral@gmail.com>
2025-08-22 09:03:10 -0300

    alteracao do codigo para executar encontrar arquivos quando usar o executavel, adicionado input para finalizar manualmente o programa

--- xlsx_json.py
+++ xlsx_json.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import logging
 import glob
+import sys
 import os
 from datetime import datetime
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-pasta = os.path.dirname(os.path.abspath(__file__))
-arquivos = glob.glob(os.path.join(pasta, 'excel', "*.xlsx"))
 
 class ProcessadorExcel:
-    def __init__(self, arquivos):
-        self.arquivos = arquivos
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        if getattr(sys, 'frozen', False):
+            self.pasta = os.path.dirname(sys.executable)
+        else:
+            self.pasta = os.path.dirname(os.path.abspath(__file__))
+            
+        self.arquivos = glob.glob(os.path.join(self.pasta, "*.xlsx"))
+
     
     def processar_linha(self, row):
         for col in row.index:
@@ -24,36 +28,37 @@ class ProcessadorExcel:
         return row
 
     def main(self):
-        for arquivo in self.arquivos:
-            logging.info(f"Lendo arquivo: {arquivo}")
+        if not self.arquivos:
+            logging.warning("Nenhum arquivo Excel (.xlsx) encontrado na self.pasta atual.")
+        else:
+            for arquivo in self.arquivos:
+                logging.info(f"Lendo arquivo: {arquivo}")
 
-            df = pd.read_excel(arquivo)
-            df = df.apply(self.processar_linha, axis=1)
-            
-            for i in range(len(df)):
-                try:
-                    logging.info(f"Linha {i} concluida")
-                except Exception as e:
-                    logging.error(f"Erro ao processar linha {i}: {e}")
-
-            json_data = df.to_json(orient="records",
-                                force_ascii=False,
-                                indent=4
-                                )
-
-            json_data = json_data.replace("\\/", "/")
-
-            nome_json = os.path.splitext(os.path.basename(arquivo))[0] + ".json"
-            caminho_json = os.path.join(pasta, 'json', nome_json)
-
-            with open(caminho_json, "w", encoding="utf-8") as file:
-                file.write(json_data)
+                df = pd.read_excel(arquivo)
+                df = df.apply(self.processar_linha, axis=1)
                 
-            print("JSON gerado com sucesso")
+                for i in range(len(df)):
+                    try:
+                        logging.info(f"Linha {i} concluida")
+                    except Exception as e:
+                        logging.error(f"Erro ao processar linha {i}: {e}")
+
+                json_data = df.to_json(orient="records",
+                                    force_ascii=False,
+                                    indent=4
+                                    )
+
+                json_data = json_data.replace("\\/", "/")
+
+                nome_json = os.path.splitext(os.path.basename(arquivo))[0] + ".json"
+                caminho_json = os.path.join(self.pasta, nome_json)
+
+                with open(caminho_json, "w", encoding="utf-8") as file:
+                    file.write(json_data)
+                    
+                print("JSON gerado com sucesso")
             
 if __name__ == "__main__":
-    if not arquivos:
-        logging.warning("Nenhum arquivo Excel (.xlsx) encontrado na pasta atual.")
-    else:
-        processador = ProcessadorExcel(arquivos)
-        processador.main()
+    processador = ProcessadorExcel()
+    processador.main()
+    input("Pressione Enter para sair...")
